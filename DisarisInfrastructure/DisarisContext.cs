595406e67ed36a8bf56d7d5847083d88ccb7dfc9
shift-eleven <81341454+shift-eleven@users.noreply.github.com>
2021-03-30 12:30:00 +0530

    Update DisarisContext.cs

--- DisarisInfrastructure/DisarisContext.cs
+++ DisarisInfrastructure/DisarisContext.cs
@@ -9,6 +9,6 @@ namespace DisarisInfrastructure
         public DbSet<Tag> Tags { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseMySql("server=localhost;user=root;password=W1@inter;database=disarisdb;port=3306;Connect Timeout=5;");
+            => options.UseMySql("server=;user=;password=;database=;port=;Connect Timeout=5;");
     }
 }
